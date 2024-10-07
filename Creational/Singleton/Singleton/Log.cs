ab9faaa961391050f6b6962594fc7269e31020cc
Facundo Zapata <zapataf@ieric.org.ar>
2024-10-07 16:15:31 -0300

    Reestructuración de proyecto + implementación de patron singleton

---
+++ Creational/Singleton/Singleton/Log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Singleton
+{
+    public sealed class Log
+    {
+        private static Log _instance;
+        private string path;
+
+        public static Log GetInstance(string path)
+        {
+            if (_instance == null) {
+                _instance = new Log(path);
+            }
+
+            return _instance;
+        }
+
+        private Log(string path) 
+        {
+            this.path = path;
+        }
+
+        public void GuardarLog(string text) 
+        {
+            File.AppendAllText(this.path, text +  Environment.NewLine);
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log log = Log.GetInstance("log.txt");
+            Log log2 = Log.GetInstance("pepe.txt");
+
+            log.GuardarLog("Log y Log2 son dos espacios en memoria diferente, ambos guardan un log, ambos son el mismo log o es una instancia diferente? RTA:" + ((log == log2) ? " Son el mismo log, el singleton hace que se trabaje con el mismo objeto asegurando que hay solo una instancia del mismo" : " Son diferentes log, entonces algo salio mal..."));
+            Console.WriteLine("Escriba algo a guardar en el log.");
+            log.GuardarLog(Console.ReadLine());
+            Console.WriteLine("Presione una tecla para terminar");
+            Console.ReadKey();
+        }
+    }
+}
